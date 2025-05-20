360714dbc7d0474b9a97d1aa1281747569a7f73e
Hmudda <normanwebdesigners@gmail.com>
2025-05-20 15:33:21 -0400

    2-page demo
    Jordan Babb

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,19 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Anthony Electric LLC",
+    "email": "anthelectric@gmail.com",
+    "phoneForTel": "918-706-8451",
+    "phoneFormatted": "(918) 706-8451",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "109 N. Garfield Ave",
+        "city": "Sand Springs",
+        "state": "OK",
+        "zip": "74063",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
+        "facebook": "https://www.facebook.com/AnthonyElectricllc/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.anthonyelectricllc.com"
 };
